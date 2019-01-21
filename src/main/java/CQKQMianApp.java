f4b0c96601abb3fa4e74c58d345da0661d7088ad
gaoEnglish <794877513@qq.com>
2019-01-21 20:49:29 +0800

    添加主料 添加消息监听类

---
+++ src/main/java/CQKQMianApp.java
@@ -0,0 +1,17 @@
+import com.mumu.webclient.KQWebClient;
+import control.Jcqapp;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class CQKQMianApp {
+    /**
+     * KQURL 酷Q本机地址
+     */
+    private static final String KQURL="ws://localhost:253030";
+    private static KQWebClient cc;
+    public static void main(String[] args) throws URISyntaxException {
+         cc= new KQWebClient(new URI(KQURL));
+         cc.addQQMSGListenner(new Jcqapp(cc));
+    }
+}
--- src/main/java/control/Jcqapp.java
+++ src/main/java/control/Jcqapp.java
@@ -1,66 +1,43 @@
 package control;
 
-import com.sobte.cqp.jcq.event.JcqAppAbstract;
+import com.mumu.listenner.KQMSGAdapter;
+import com.mumu.webclient.KQWebClient;
 
-public class Jcqapp extends JcqAppAbstract{
-
-    public int startup() {
-        return 0;
+public class Jcqapp extends KQMSGAdapter{
+    private KQWebClient cc;
+    public Jcqapp(KQWebClient cc){
+        this.cc=cc;
+    }
+    //以下是会自动调用的方法
+    /**
+     * 私聊消息处理
+     * 有私聊消息自动触发
+     * @param msg
+     */
+    public void Re_MSG_Private(com.mumu.msg.RE_MSG_Private msg) {
+        super.Re_MSG_Private(msg);
+        //参数1 :需要发送的qq ,参数2:发送的消息
+        cc.sendPrivateMSG(msg.getFromqq(),"你发送了: "+msg.getMsg());
     }
 
-    public int exit() {
-        return 0;
+    /**
+     * 讨论组消息处理
+     * 有讨论组消息自动触发
+     * @param msg
+     */
+    public void RE_MSG_FORUM(com.mumu.msg.RE_MSG_Forum msg) {
+        super.RE_MSG_FORUM(msg);
     }
 
-    public int enable() {
-        return 0;
+    /**
+     * 群消息处理
+     * 有群消息消息自动触发
+     * @param msg
+     */
+    public void RE_MSG_Group(com.mumu.msg.RE_MSG_Group msg) {
+        super.RE_MSG_Group(msg);
+        //qq:需要@的qq,groupid:发送的群号，msg :发送的消息 ,isAT: 是否需要@发送 true是 false否
+        cc.sendGroupMSG("","100936163","@并发送了: "+msg.getMsg(),true);
     }
 
-    public int disable() {
-        return 0;
-    }
-
-    public int privateMsg(int i, int i1, long l, String s, int i2) {
-        return 0;
-    }
-
-    public int groupMsg(int i, int i1, long l, long l1, String s, String s1, int i2) {
-        return 0;
-    }
-
-    public int discussMsg(int i, int i1, long l, long l1, String s, int i2) {
-        return 0;
-    }
-
-    public int groupUpload(int i, int i1, long l, long l1, String s) {
-        return 0;
-    }
-
-    public int groupAdmin(int i, int i1, long l, long l1) {
-        return 0;
-    }
-
-    public int groupMemberDecrease(int i, int i1, long l, long l1, long l2) {
-        return 0;
-    }
-
-    public int groupMemberIncrease(int i, int i1, long l, long l1, long l2) {
-        return 0;
-    }
-
-    public int friendAdd(int i, int i1, long l) {
-        return 0;
-    }
-
-    public int requestAddFriend(int i, int i1, long l, String s, String s1) {
-        return 0;
-    }
-
-    public int requestAddGroup(int i, int i1, long l, long l1, String s, String s1) {
-        return 0;
-    }
-
-    public String appInfo() {
-        return null;
-    }
 }
