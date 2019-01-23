b6d10e21141a7a67620831d257b1580313e0dce3
lossoftime <46859755+lossoftime@users.noreply.github.com>
2019-01-23 17:25:37 +0800

    完善备注
    回答时会@提问者

--- src/main/java/controller/CQMSGAdapter.java
+++ src/main/java/controller/CQMSGAdapter.java
@@ -38,15 +38,38 @@ public class CQMSGAdapter extends KQMSGAdapter{
      */
     public void RE_MSG_Group(com.mumu.msg.RE_MSG_Group msg) {
         super.RE_MSG_Group(msg);
-        System.out.println("\033[36;0m" +"从群号: "+"\033[33;0m"+msg.getFromGroup()+"("+msg.getFromGroupName()+")"+ "\033[36;0m" + " [接收_"+msg.getNick()+"("+msg.getUsername()+")_消息] : "+"\033[31;0m"+msg.getMsg()+ "\033[0m");
-        if(msg.getMsg().indexOf("[CQ:at")<0){
-            return;
+        
+        System.out.println("\033[36;0m" +"从群号: "+"\033[33;0m"+msg.getFromGroup()+"("+msg.getFromGroupName()+")"+ "\033[36;0m" + " [接收_"+msg.getFromQQ()+"("+msg.getUsername()+")_消息] : "+"\033[31;0m"+msg.getMsg()+ "\033[0m");
+        
+        //只监听该群
+        if(!msg.getFromGroup().toString().equals("189309830"))
+        {	
+        	System.out.println("群不一致");
+        	return;
         }
-        if(msg.getMsg().indexOf("qq=2426076950")<0){
+        
+        //识别@符号
+        if(msg.getMsg().indexOf("[CQ:at")<0)	
+        {
+        	System.out.println("需要at");
+        	return;
+        }
+        //@他才行
+        if(msg.getMsg().indexOf("qq=210889912")<0)	
+        {
+        	System.out.println("需要at他才行");
+        	return;
+        }
+        
+        //忽略图片和表情
+        if(msg.getMsg().indexOf("[CQ:image")>=0||msg.getMsg().indexOf("[CQ:emoji")>=0){
             return;
         }
         //qq:需要@的qq,groupid:发送的群号，msg :发送的消息 ,isAT: 是否需要@发送 true是 false否
-        cc.sendGroupMSG("","100936163",msg.getMsg().split("]")[1],false);
+        cc.sendGroupMSG(msg.getFromQQ(),msg.getFromGroup(),msg.getMsg().split("]")[1],true);
+        //qq为""则不会返回@，为msg.getFromQQ()则@提问者
+        //groupid为msg.getFromGroup() ，返回所有群；为群号字符串则只回答该群
+        //msg为msg.getMsg()  ，复读
     }
 
 }
