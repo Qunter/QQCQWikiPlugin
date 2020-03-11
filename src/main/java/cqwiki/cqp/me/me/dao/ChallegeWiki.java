d18a7b00463e97569278da30f4536b7fa932788b
刘典昆 <liudiankun@galasports.net>
2020-03-11 14:16:17 +0800

    删除多余文件

--- src/main/java/cqwiki/cqp/me/me/dao/ChallegeWiki.java
+++
@@ -1,9 +0,0 @@
-package cqwiki.cqp.me.dao;
-
-public interface ChallegeWiki {
-	String getTeam_data(String name);
-
-	String getTeamDetail_data(String chaname, int index);
-	
-	String getBoss_data(String name,int index);
-}
